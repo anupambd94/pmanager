4366cfdfb02745ddd4ed31cacf4872e6a22e7bf5
Anupam Hayat <anupam.bd94@gmail.com>
2018-09-23 13:58:32 +0600

    added more attributes in task table and added the foreign keys

--- database/migrations/2018_09_23_075158_create_tasks_table.php
+++ database/migrations/2018_09_23_075158_create_tasks_table.php
@@ -15,6 +15,11 @@ class CreateTasksTable extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
+            $table->integer('project_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            $table->foreign('project_id')->references('id')->on('projects');
+            $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
